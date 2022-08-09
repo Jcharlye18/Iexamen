8e7e63dd1f6a42bd9c4e9de8155b4e2e397cda61
Jcharlye18 <jc17arlos@gmail.com>
2022-08-08 20:30:48 -0600

    Mi Primer Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="tabla.html">Tablas</a></li>
+                <li><a href="#">Formulario</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main class="contenedor1">
+        <div class="contenedor2">
+            <div class="img">
+                <img src="codigo.jpg" alt="">
+            </div>
+            <div class="texto">
+                <h1 class="titulo">BIENVENIDO</h1>
+                <p class="parrafo">Al examen parcial de desarrollo Web, en las paginas que usted vera aqui podra ver el
+                    avance que hemos tenido en las primeras semanas de clase...</p>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <h2 class="titulo2">Todos los derechos reservados a /UMG/</h2>
+    </footer>
+</body>
+
+</html>
